80017058bd6a124d358ab4ff2cd317f36d9ee3c1
Sonia Khan <B00155615@mytudublin.ie>
2024-04-14 21:33:25 +0100

    Login Added

---
+++ www/login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    
+    <?php
+ 
+?>
+    <head>
+       
+
+       
+        <link rel="stylesheet"
+          href="CSS/login.css">
+
+      
+           
+        
+    </head>
+    <body>
+    <fieldset>
+            
+        <h1> Login</h1>
+        <br><br><br><br><br>
+    <form  method="post"  action="">
+  <label for="email">Email:</label> &nbsp;&nbsp;&nbsp;&nbsp;
+  <input type="text" id="email" name="email" value="" > <br> <br>
+        
+         
+           
+        <br><br>
+        
+ <label for="address">Password:</label>
+  <input type="password" id="address" name="address" ><br><br> &nbsp;&nbsp;&nbsp;
+       
+        <p>need an account?</p> <a3 href="signup.php">Signup</a3>
+        
+        <br><br>
+        
+        
+    <a href="index.php" class="button">Login</a>
+      
+        
+
+
+        
+ 
+
+ 
+    </form>
+        
+     </fieldset>
+</body>
+</html>
